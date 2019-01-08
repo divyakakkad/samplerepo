c653930cc26d6b8dfa6dfed4729273f29c4c50e0
divyakakkad <46053969+divyakakkad@users.noreply.github.com>
2019-01-08 14:34:54 +0530

    Add files via upload

---
+++ hellowor.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class hellowor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
